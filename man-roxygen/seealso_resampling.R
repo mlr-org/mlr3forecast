1507fb11135d6d54cf9ffbaf0e22bbf105cb5edc
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-08-10 12:45:45 +0200

    docs: update seealso_resampling template from latest mlr3

--- man-roxygen/seealso_resampling.R
+++ man-roxygen/seealso_resampling.R
@@ -1,8 +1,11 @@
 #' @seealso
-#'
-#' * [as_benchmark_result()] to convert to a [BenchmarkResult].
+#
 #' * Chapter in the [mlr3book](https://mlr3book.mlr-org.com/):
-#'   \url{https://mlr3book.mlr-org.com/performance.html#sec-resampling}
-#' * Package \CRANpkg{mlr3viz} for some generic visualizations.
+#'   \url{https://mlr3book.mlr-org.com/chapters/chapter3/evaluation_and_benchmarking.html#sec-resampling}
+#' * Package \CRANpkg{mlr3spatiotempcv} for spatio-temporal resamplings.
+#' * [Dictionary][mlr3misc::Dictionary] of [Resamplings][Resampling]: [mlr_resamplings]
+#' * `as.data.table(mlr_resamplings)` for a table of available [Resamplings][Resampling] in the running session (depending on the loaded packages).
+#' * \CRANpkg{mlr3spatiotempcv} for additional [Resampling]s for spatio-temporal
+#'   tasks.
 #'
-#' @family resample
+#' @family Resampling
