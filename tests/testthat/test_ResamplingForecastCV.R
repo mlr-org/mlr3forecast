24130913574517484b8ab2d95c7ffc5aef87f2eb
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-08-13 22:47:17 +0200

    tests: more tests for resampling

--- tests/testthat/test_ResamplingForecastCV.R
+++ tests/testthat/test_ResamplingForecastCV.R
@@ -35,3 +35,57 @@ test_that("forecast_cv works", {
   walk(1:3, function(i) expect_length(resampling$test_set(i), 5L))
   walk(0:2, function(i) expect_identical(resampling$train_set(i + 1L), (115L - i):(139L - i)))
 })
+
+test_that("forecast_cv fixed vs. expanding window", {
+  task = tsk("penguins")
+  task$filter(1:30)
+
+  # fixed window
+  resampling = rsmp("forecast_cv",
+    folds = 3L, horizon = 3L, window_size = 5L, fixed_window = TRUE
+  )
+  resampling$instantiate(task)
+  expect_identical(resampling$train_set(1L), 23:27)
+  expect_identical(resampling$train_set(2L), 22:26)
+  expect_identical(resampling$train_set(3L), 21:25)
+
+  # expanding window
+  resampling = rsmp("forecast_cv",
+    folds = 3L, horizon = 3L, window_size = 5L, fixed_window = FALSE
+  )
+  resampling$instantiate(task)
+  expect_identical(resampling$train_set(1L), 1:27)
+  expect_identical(resampling$train_set(2L), 1:26)
+  expect_identical(resampling$train_set(3L), 1:25)
+})
+
+test_that("forecast_cv with various parameter combinations", {
+  task = tsk("penguins")
+  task$filter(1:30)
+
+  # small window, large step size
+  resampling = rsmp("forecast_cv",
+    folds = 5L, horizon = 2L, window_size = 3L, step_size = 2L, fixed_window = TRUE
+  )
+  resampling$instantiate(task)
+  expect_identical(resampling$train_set(1L), 26:28)
+  expect_identical(resampling$test_set(1L), 29:30)
+  expect_identical(resampling$train_set(2L), 24:26)
+  expect_identical(resampling$test_set(2L), 27:28)
+  expect_identical(resampling$train_set(3L), 22:24)
+  expect_identical(resampling$test_set(3L), 25:26)
+
+  # large window, small horizon
+  resampling = rsmp("forecast_cv",
+    folds = 4L, horizon = 1L, window_size = 10L, step_size = 1L, fixed_window = FALSE
+  )
+  resampling$instantiate(task)
+  expect_identical(resampling$train_set(1L), 1:29)
+  expect_identical(resampling$test_set(1L), 30L)
+  expect_identical(resampling$train_set(2L), 1:28)
+  expect_identical(resampling$test_set(2L), 29L)
+  expect_identical(resampling$train_set(3L), 1:27)
+  expect_identical(resampling$test_set(3L), 28L)
+  expect_identical(resampling$train_set(4L), 1:26)
+  expect_identical(resampling$test_set(4L), 27L)
+})
--- tests/testthat/test_ResamplingForecastHoldout.R
+++ tests/testthat/test_ResamplingForecastHoldout.R
@@ -26,3 +26,24 @@ test_that("forecast_holdout works", {
   expect_identical(resampling$train_set(1L), 1:72)
   expect_identical(resampling$test_set(1L), 73:144)
 })
+
+test_that("forecast_holdout repeated instantiation", {
+  task = tsk("penguins")
+
+  resampling = rsmp("forecast_holdout", ratio = 0.6)
+  resampling$instantiate(task)
+  train_set_1 = resampling$train_set(1L)
+  test_set_1 = resampling$test_set(1L)
+
+  resampling$instantiate(task)
+  train_set_2 = resampling$train_set(1L)
+  test_set_2 = resampling$test_set(1L)
+
+  expect_identical(train_set_1, train_set_2)
+  expect_identical(test_set_1, test_set_2)
+
+  resampling$param_set$values$ratio = 0.8
+  resampling$instantiate(task)
+  expect_false(identical(train_set_1, resampling$train_set(1L)))
+  expect_false(identical(test_set_1, resampling$test_set(1L)))
+})
