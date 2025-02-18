164bb787d6fd5ddba330f1e3e3888c901b49d1ea
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-02-18 23:26:30 +0100

    chore: bump to R 3.6 since latest R6 requires 3.6

--- R/zzz.R
+++ R/zzz.R
@@ -25,7 +25,7 @@ named_union = function(x, y) set_names(union(x, y), union(names(x), names(y)))
 
 register_item = function(env, type) {
   function(name, constructor) {
-    if (hasName(env, name)) stopf("%s %s registered twice", type, name)
+    if (utils::hasName(env, name)) stopf("%s %s registered twice", type, name)
     env[[name]] = constructor
   }
 }
@@ -97,7 +97,6 @@ register_mlr3pipelines = function() {
 
 .onLoad = function(libname, pkgname) {
   backports::import(pkgname)
-  backports::import(pkgname, "hasName", force = TRUE)
 
   assign("lg", lgr::get_logger("mlr3"), envir = parent.env(environment()))
   if (Sys.getenv("IN_PKGDOWN") == "true") {
