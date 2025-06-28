6bd59498ce38c0e4f45f989ac62f710c3a768d14
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-06-28 10:42:48 +0200

    docs: add missing entry for allowed freq values

--- man-roxygen/param_freq.R
+++ man-roxygen/param_freq.R
@@ -1,6 +1,7 @@
 #' @param freq (`character(1)` | `integer(1)`)\cr
 #'   Frequency of the time series.
 #'   Either a positive number or one of the following:
+#'   * `"secondly"`
 #'   * `"minutely"`
 #'   * `"hourly"`
 #'   * `"daily"`
