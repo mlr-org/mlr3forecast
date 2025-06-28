d7020a78a7c0924ce4cf9a1bc7f164f86624d26c
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-06-28 10:20:08 +0200

    docs: more line breaks

--- man-roxygen/param_freq.R
+++ man-roxygen/param_freq.R
@@ -1,6 +1,6 @@
 #' @param freq (`character(1)` | `integer(1)`)\cr
 #'   Frequency of the time series.
-#'   Either a positive number or one of the following:
+#'   Either a positive number or one of the following:\cr
 #'     * `"minutely"`
 #'     * `"hourly"`
 #'     * `"daily"`
