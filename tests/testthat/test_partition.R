0b6cef9525e9fc0cb0430e52eb95e413edc70fee
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-03-26 13:27:00 +0100

    test: add test for keyed partition call

--- tests/testthat/test_partition.R
+++ tests/testthat/test_partition.R
@@ -24,3 +24,14 @@ test_that("partition preserves temporal order", {
   li = partition(task, ratio = 0.67)
   expect_true(max(li$train) < min(li$test))
 })
+
+test_that("partition works with keyed task", {
+  b = as_data_backend(load_dataset("Orange", "datasets"))
+  task = TaskFcst$new(id = "orange", backend = b, target = "circumference", order = "age", key = "Tree")
+  li = partition(task, ratio = 0.67)
+  expect_list(li, len = 3L)
+  expect_names(names(li), identical.to = c("train", "test", "validation"))
+  expect_equal(length(li$train) + length(li$test) + length(li$validation), task$nrow)
+  expect_length(li$validation, 0L)
+  expect_disjunct(li$train, li$test)
+})
