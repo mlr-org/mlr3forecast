d3b8c6c97135df96d5cf2385811839619f2fd843
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-04-07 21:40:45 +0200

    feat: add quantile support for tscount

--- R/LearnerFcstTscount.R
+++ R/LearnerFcstTscount.R
@@ -39,13 +39,14 @@ LearnerFcstTscount = R6Class(
           custom_check = crate(function(x) check_logical(x))
         ),
         link = p_fct(c("identity", "log"), default = "identity", tags = "train"),
-        distr = p_fct(c("poisson", "nbinom"), default = "poisson", tags = "train")
+        distr = p_fct(c("poisson", "nbinom"), default = "poisson", tags = "train"),
+        B = p_int(10L, default = 1000L, tags = "predict")
       )
 
       super$initialize(
         id = "fcst.tscount",
         param_set = param_set,
-        predict_types = "response",
+        predict_types = c("response", "quantiles"),
         feature_types = c("logical", "integer", "numeric"),
         properties = c("featureless", "exogenous"),
         packages = c("mlr3forecast", "tscount"),
@@ -89,9 +90,13 @@ LearnerFcstTscount = R6Class(
     },
 
     .predict = function(task) {
+      is_quantile = self$predict_type == "quantiles"
       prediction = list(extra = as.list(task$data(cols = task$col_roles$order)))
 
       if (!private$.is_newdata(task)) {
+        if (is_quantile) {
+          error_config("Quantile prediction not supported for in-sample prediction.")
+        }
         response = stats::fitted(self$model)[task$row_ids]
         return(insert_named(prediction, list(response = response)))
       }
@@ -101,8 +106,34 @@ LearnerFcstTscount = R6Class(
         newxreg = as.matrix(task$data(cols = task$feature_names))
       }
 
-      pred = stats::predict(self$model, n.ahead = task$nrow, newxreg = newxreg, level = 0)
-      insert_named(prediction, list(response = as.numeric(pred$pred)))
+      pv = self$param_set$get_values(tags = "predict")
+      n_ahead = task$nrow
+
+      pred = stats::predict(self$model, n.ahead = n_ahead, newxreg = newxreg, level = 0)
+      mu = as.numeric(pred$pred)
+
+      if (!is_quantile) {
+        return(insert_named(prediction, list(response = mu)))
+      }
+
+      probs = private$.quantiles
+      if (n_ahead == 1L) {
+        quantiles = if (self$model$distr == "poisson") {
+          vapply(probs, function(p) qpois(p, lambda = mu), numeric(1L))
+        } else {
+          vapply(probs, function(p) qnbinom(p, size = self$model$distrcoefs, mu = mu), numeric(1L))
+        }
+        quantiles = matrix(quantiles, nrow = 1L)
+      } else {
+        B = pv$B %??% 1000L
+        futureobs = replicate(B, {
+          tscount::tsglm.sim(n = n_ahead, fit = self$model, xreg = newxreg, n_start = 0L)$ts
+        })
+        quantiles = vapply(probs, function(p) apply(futureobs, 1L, quantile, probs = p, type = 1L), numeric(n_ahead))
+      }
+      setattr(quantiles, "probs", probs)
+      setattr(quantiles, "response", private$.quantile_response)
+      insert_named(prediction, list(quantiles = quantiles))
     }
   )
 )
