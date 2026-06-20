378f8b4706f90c505919c93009196d084ebf82bb
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-06-20 13:17:28 +0200

    feat: add fcstavg pipe op

---
+++ tests/testthat/test_PipeOpFcstAvg.R
@@ -0,0 +1,53 @@
+test_that("PipeOpFcstAvg keeps PredictionFcst and infers fcst task_type", {
+  skip_if_not_installed("forecast")
+  task = tsk("airpassengers")
+  graph = gunion(list(
+    po("learner", lrn("fcst.ets"), id = "ets"),
+    po("learner", lrn("fcst.theta"), id = "theta")
+  )) %>>%
+    po("fcst.regravg")
+  glrn = as_learner(graph)
+  expect_equal(glrn$task_type, "fcst")
+
+  glrn$train(task)
+  p = forecast(glrn, task, 6L)
+  expect_r6_class(p, "PredictionFcst")
+  expect_equal(nrow(p$order), 6L)
+})
+
+test_that("PipeOpFcstAvg response is the row-wise weighted average", {
+  skip_if_not_installed("forecast")
+  task = tsk("airpassengers")
+  l1 = lrn("fcst.ets")
+  l2 = lrn("fcst.theta")
+  graph = gunion(list(
+    po("learner", l1$clone(), id = "ets"),
+    po("learner", l2$clone(), id = "theta")
+  )) %>>%
+    po("fcst.regravg")
+
+  p = forecast(as_learner(graph)$train(task), task, 6L)
+  p1 = forecast(l1$clone()$train(task), task, 6L)
+  p2 = forecast(l2$clone()$train(task), task, 6L)
+  expect_equal(p$response, (p1$response + p2$response) / 2)
+})
+
+test_that("PipeOpFcstAvg preserves keys for multi-series tasks", {
+  dt = rbind(
+    data.table(series = "a", t = as.Date("2020-01-01") + 0:23, y = as.numeric(1:24)),
+    data.table(series = "b", t = as.Date("2020-01-01") + 0:23, y = as.numeric(24:1))
+  )
+  dt[, series := as.factor(series)]
+  task = as_task_fcst(dt, target = "y", order = "t", key = "series", freq = "day")
+  fg = po("fcst.lags", lags = 1:3) %>>% lrn("regr.featureless")
+  graph = gunion(list(
+    po("learner", recursive_forecaster(fg), id = "f1"),
+    po("learner", recursive_forecaster(fg, lags = 1:2), id = "f2")
+  )) %>>%
+    po("fcst.regravg")
+
+  p = as_learner(graph)$train(task)$predict(task, task$row_ids[c(22:24, 46:48)])
+  expect_r6_class(p, "PredictionFcst")
+  expect_equal(names(p$key), c("row_id", "key"))
+  expect_equal(as.character(p$key$key), c("a", "a", "a", "b", "b", "b"))
+})
