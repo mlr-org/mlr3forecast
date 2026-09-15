03b13b8ead712586cd5361699d536ec560530015
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-09-15 22:05:54 +0200

    fix: correct export

--- R/as_task_fcst.R
+++ R/as_task_fcst.R
@@ -162,7 +162,7 @@ as_task_fcst.tsf = function(x, id = deparse1(substitute(x)), label = NA_characte
 }
 
 #' @rdname as_task_fcst
-#' @exportS3Method mlr3::as_task
+#' @export
 as_task.tsf = function(x, id = deparse1(substitute(x)), clone = FALSE, ...) {
   force(id)
   as_task_fcst(x, id = id, ...)
