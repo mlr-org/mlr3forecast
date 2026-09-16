1fa9055baa1be8babf42ed8642ad6b852d94f19e
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-09-16 19:10:13 +0200

    test: remove forecTheta nolint directives

--- tests/testthat/test_fcst_stheta.R
+++ tests/testthat/test_fcst_stheta.R
@@ -3,7 +3,6 @@ skip_if_not_installed("forecTheta")
 test_that("autotest", {
   learner = lrn("fcst.stheta")
   expect_learner(learner)
-  # nolint next: unreachable_code_linter.
   if (FALSE) {
     result = run_autotest(learner)
     expect_true(result, info = result$error)
